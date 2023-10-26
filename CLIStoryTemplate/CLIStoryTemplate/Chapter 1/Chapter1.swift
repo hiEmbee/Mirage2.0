4b49ccb3f4a2775a07e69b2e8d18428e4aa11e8c
lizmaryf <lizmaryfernandez7@gmail.com>
2023-10-26 09:29:59 -0400

    updated chapters 1 & 2

--- CLIStoryTemplate/CLIStoryTemplate/Chapter 1/Chapter1.swift
+++ CLIStoryTemplate/CLIStoryTemplate/Chapter 1/Chapter1.swift
@@ -4,7 +4,10 @@
 //
 
 import Foundation
+var hotel = "Hotel Mirage"
 
 func chapterOne() {
-    // Your portion of the story goes here
+    print("It is October 18th, 1931 and it’s been 2 years since the stock market crashed causing a huge economic depression in the United States. Times are so tough that even the banks are going out of business. To make matters worse there are flesh eating monsters wandering the streets. We call them zombies. They are vicious and will destroy everything and everyone that gets in their way. Out of fear of the unemployed, many had to find shelter and quarantine. That’s what brought us to Hotel Mirage. It was the only \(hotel) available within a 100 mile radius.")
 }
+
+    
--- CLIStoryTemplate/CLIStoryTemplate/Chapter 2/Chapter2.swift
+++ CLIStoryTemplate/CLIStoryTemplate/Chapter 2/Chapter2.swift
@@ -4,7 +4,19 @@
 //
 
 import Foundation
+var nameA = "Malaya"
+var ageA = 25
+var weightA = "160"
+var heightA = 6
+
 
 func chapterTwo() {
-    // Your portion of the story goes here
+    print("Patient: \(nameA) Age : \(ageA) Weight :\(weightA) height:\(heightA)")
 }
+func introduceMalaya() {
+    print("My name is Malaya.")
+    print("I love to play tennis.")
+    print("If I could describe myself in one word it would be pessismist")
+    print("My favorite color is yellow")
+}
+
