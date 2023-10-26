22c99e94e82ae17a5dc183d7415cdeb63044e1f7
Embee <mabintybangoura214@gmail.com>
2023-10-26 09:33:25 -0400

    added chapter1 welcome

--- CLIStoryTemplate/CLIStoryTemplate/Chapter 1/Chapter1.swift
+++ CLIStoryTemplate/CLIStoryTemplate/Chapter 1/Chapter1.swift
@@ -7,4 +7,24 @@ import Foundation
 
 func chapterOne() {
     // Your portion of the story goes here
+    let greeting = "Hello, Playground"
+    
+    print("Enter Character Choice:")
+    let name = readLine()
+    
+    print("Your charecter pov is \(name!).")
+    
+    print("Where do you want to go?")
+    var destination = readLine()
+    
+    if destination == "chapterTwo" {
+        chapterTwo()
+    }
+    else {
+        print("I cant go there.")
+    }
+    
+    
+ 
 }
+
--- CLIStoryTemplate/CLIStoryTemplate/Chapter 2/Chapter2.swift
+++ CLIStoryTemplate/CLIStoryTemplate/Chapter 2/Chapter2.swift
@@ -7,4 +7,5 @@ import Foundation
 
 func chapterTwo() {
     // Your portion of the story goes here
+    print("Welcome to chapter 2")
 }
