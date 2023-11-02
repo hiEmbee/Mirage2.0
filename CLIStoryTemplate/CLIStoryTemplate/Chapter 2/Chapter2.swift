1b7baaad93c36211a9c524a4598402cb7b67bcf8
lizmaryf <lizmaryfernandez7@gmail.com>
2023-11-02 08:41:03 -0400

    struct patient file

--- CLIStoryTemplate/CLIStoryTemplate/Chapter 2/Chapter2.swift
+++ CLIStoryTemplate/CLIStoryTemplate/Chapter 2/Chapter2.swift
@@ -16,8 +16,6 @@ let resources = ["food", "water", "soap", "love", "attention"]
 let randomResources = resources.randomElement() ?? ""
 
 
-
-
 func chapterTwo() {
     print("Patient: \(nameA) DOB: \(bDate) Age : \(ageA) Weight :\(weightA) height:\(heightA)")
     
@@ -32,10 +30,11 @@ func chapterTwo() {
         print(". My entire existence I have been running away from evil.  I ran the fastest from Brutus: evil personified, and somehow my father. Despite having 4 older siblings I was the only one he took his anger out on.  I was rebellious because I loved adventuring outdoors. There were just so many possibilities in the world. I could be so much more if I could just get out of these 4 walls my family calls home. In my many adventures I would often be up to mischievous things.")
     }
     func tellJoke(setup: String) -> String {
-        if setup == "What do you call a bear with no teeth?" {
+        if setup == "What do you call a bear with no teeth? Type I don't know to continue" {
             return "A gummy bear!"
         }else{
             return "I don't know"
+            print("A gummy bear!")
         }
     }
     introduceMalaya()
