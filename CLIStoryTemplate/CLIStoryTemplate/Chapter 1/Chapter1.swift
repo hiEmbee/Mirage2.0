15f1012e0e97e03c69f87286f9d2af83deefd5f3
lizmaryf <lizmaryfernandez7@gmail.com>
2023-11-07 10:12:18 -0500

    ch. 1 & 2 added schedule

--- CLIStoryTemplate/CLIStoryTemplate/Chapter 1/Chapter1.swift
+++ CLIStoryTemplate/CLIStoryTemplate/Chapter 1/Chapter1.swift
@@ -14,40 +14,69 @@ func chapterOne() {
     print("It is October 18th, 1931 and it’s been 2 years since the stock market crashed causing a huge economic depression in the United States. Times are so tough that even the banks are going out of business. To make matters worse there are flesh eating monsters wandering the streets. We call them zombies. They are vicious and will destroy everything and everyone that gets in their way. Out of fear of the unemployed, many had to find shelter and quarantine. That’s what brought us to Hotel Mirage. It was the only \(hotel) available within a 100 mile radius.")
     print("It is October 18th, 1931, and the world is in chaos. The stock market crashed two years ago, causing a massive economic depression in the United States. To make matters worse, flesh-eating monsters, known as zombies, are wandering the streets, destroying everything and everyone in their path. In the midst of this apocalypse, a group of people has found refuge at Hotel Mirage, the only hotel available within a 100-mile radius.")
     print("At Hotel Mirage, the staff and guests have formed a tight-knit family unit. They have bonded through the zombie apocalypse, as resources are scarce, and they need to maximize their efficiency and delegate different tasks. Some resources that are scarce include natural gas, which is needed to cook, so they have to make a fire every time they want to cook. A few people are in charge of cooking meals, while others wash their clothes. To keep their sanity, they have developed a very detailed schedule that they follow strictly.The situation at Hotel Mirage is dire, but the staff and guests have come together to survive. They have learned to rely on each other and work together to overcome the challenges they face. The zombie apocalypse has forced them to adapt and be resourceful, and they have risen to the occasion.")
-          print("In times of crisis, it is essential to come together and support each other. The staff and guests at Hotel Mirage have shown that even in the face of a zombie apocalypse, it is possible to find hope and strength in each other. They have formed a family unit that has allowed them to survive and thrive in the midst of chaos. As we face our own challenges in life, let us remember the lessons we can learn from the staff and guests at Hotel Mirage. Let us come together, support each other, and work towards a common goal. Together, we can overcome any obstacle and emerge stronger on the other side.")
+    print("In times of crisis, it is essential to come together and support each other. The staff and guests at Hotel Mirage have shown that even in the face of a zombie apocalypse, it is possible to find hope and strength in each other. They have formed a family unit that has allowed them to survive and thrive in the midst of chaos. As we face our own challenges in life, let us remember the lessons we can learn from the staff and guests at Hotel Mirage. Let us come together, support each other, and work towards a common goal. Together, we can overcome any obstacle and emerge stronger on the other side.")
     
-    struct PatientFile {
-        var name: String
-        var age: Int
-        var height: Double
-        var kin: String
-        var hobbies: String
-        var specialItem: String
-        
-        func describe() {
-            print("Patient name: \(name), Age: \(age), Height: \(height), Hobbies: \(hobbies), Secret weapon: \(specialItem) ")
+    enum Schedule: Int{
+        case sevenAM,eightAM,eightthirtyAM,nineAM, tenAM, elevenAM, twelveAM, onePM, onethirtyPM, threethirtyPM, fivePM, sixPM, sevenPM, seventhrityPM, ninePM
+    }
+    func showSchedule(currentSchedule: Schedule) {
+        switch currentSchedule {
+        case .sevenAM:
+            print("Wake up")
+        case .eightAM:
+            print("Breakfast cooking group")
+        case .eightthirtyAM:
+            print("Breakfast")
+        case .nineAM:
+            print("Morning break")
+        case .tenAM:
+            print("Gardening")
+        case .elevenAM:
+            print("Individual Therapy")
+        case .twelveAM:
+            print("Lunch cooking group")
+        case .onePM :
+            print("Lunch")
+        case .onethirtyPM :
+            print("afternoon break")
+        case .threethirtyPM :
+            print("Arts & crafts")
+        case .fivePM :
+            print("Group therapy")
+        case .sixPM :
+            print("Dinner cooking group")
+        case .sevenPM :
+            print("Dinner")
+        case .seventhrityPM :
+            print("night time break")
+        case .ninePM :
+            print("Bedtime")
         }
     }
-    let malaya = PatientFile(name:"Malaya",age: 25, height: 6.1, kin: "Brutus: father, Adeline: mother", hobbies: "crochet", specialItem: "Corset")
-    let theo = PatientFile(name:"Theo",age: 20, height: 5.9, kin:"n/a", hobbies: "running", specialItem: "Medallion necklace")
-    let olivia = PatientFile(name:"Olivia",age: 32, height: 5.8, kin: "mom", hobbies: "puzzles", specialItem: "")
+    showSchedule(currentSchedule: .eightAM)
+    
+    
    
-    malaya.describe()
-    theo.describe()
-    olivia.describe()
-    
-    
-    
-    
-    
-    
-
-    
-
-    
-    
-    
-
+//    struct PatientFile {
+//        var name: String
+//        var age: Int
+//        var height: Double
+//        var kin: String
+//        var hobbies: String
+//        var specialItem: String
+//        
+//        func describe() {
+//            print("Patient name: \(name), Age: \(age), Height: \(height), Hobbies: \(hobbies), Secret weapon: \(specialItem) ")
+//        }
+//    }
+//    let malaya = PatientFile(name:"Malaya",age: 25, height: 6.1, kin: "Brutus: father, Adeline: mother", hobbies: "crochet", specialItem: "Corset")
+//    let theo = PatientFile(name:"Theo",age: 20, height: 5.9, kin:"n/a", hobbies: "running", specialItem: "Medallion necklace")
+//    let olivia = PatientFile(name:"Olivia",age: 32, height: 5.8, kin: "mom", hobbies: "puzzles", specialItem: "")
+//   
+//    malaya.describe()
+//    theo.describe()
+//    olivia.describe()
+//    
+//    
 }
-
     
--- CLIStoryTemplate/CLIStoryTemplate/Chapter 2/Chapter2.swift
+++ CLIStoryTemplate/CLIStoryTemplate/Chapter 2/Chapter2.swift
@@ -9,18 +9,19 @@ var ageA = 25
 var weightA = "160"
 var heightA = 6
 //var joke = "What do you call a bear with no teeth?"
-//var punchLine = "A gummy bear"
+var Gummy = "A gummy bear!"
 var bDate = 11/03/1906
 //var action: String
-var joke = "What do you call a bear with no teeth? Type I don't know to continue"
+let jokes = ["What do you call a bear with no teeth?"]
 
-let resources = ["food", "water", "soap", "love", "attention"]
+let resources = ["support", "love", "attention"]
 let randomResources = resources.randomElement() ?? ""
+
+let Choice = resources.randomElement() ?? ""
 //
 
 func chapterTwo() {
-    
-        struct PatientFile {
+    struct PatientFile {
             var name: String
             var age: Int
             var height: Double
@@ -31,64 +32,134 @@ func chapterTwo() {
                 print("Patient name: \(name), Age: \(age), Height: \(height), Hobbies: \(hobbies), Secret weapon: \(specialItem) ")
             }
         }
-            let malaya = PatientFile(name:"Malaya",age: 25, height: 6.1, kin: "Brutus: father, Adeline: mother", hobbies: "crochet", specialItem: "Corset")
-    
+        let malaya = PatientFile(name:"Malaya",age: 25, height: 6.1, kin: "Brutus: father, Adeline: mother", hobbies: "crochet, tennis, hiking", specialItem: "Corset")
+        
         malaya.describe()
     
-        func introduceMalaya() {
+    
+    func introduceMalaya() {
             print("My name is Malaya.")
             print("I love to play tennis.")
             print("If I could describe myself in one word it would be pessismist")
             print("My favorite color is yellow")
         }
-        func childhoodMalaya() {
+    introduceMalaya()
     
-            print(". My entire existence I have been running away from evil.  I ran the fastest from Brutus: evil personified, and somehow my father. Despite having 4 older siblings I was the only one he took his anger out on.  I was rebellious because I loved adventuring outdoors. There were just so many possibilities in the world. I could be so much more if I could just get out of these 4 walls my family calls home. In my many adventures I would often be up to mischievous things.")
-        }
-    
-    //    var action: String
-    //        var joke = "What do you call a bear with no teeth? Type I don't know to continue"
-    
-//        if joke == "I don't know" {
-//            action = ("A gummy bear!")
-//        } else if joke == "What?" {
-//            action = ("A gummy bear!")
-//        }
-    
-    
-    //    func tellJoke(setup: String) -> String {
-    //        if setup == "What do you call a bear with no teeth? Type I don't know to continue" {
-    //            return "A gummy bear!"
-    //        }else{
-    //            return "I don't know"
-    //            print("A gummy bear!")
-    //        }
-    //    }
-        introduceMalaya()
-        print("This Zombie apocalypse is very triggering for me to say the least. The lack of resources makes me think about my childhood.")
-    
-        if let resources = resources.randomElement() {
-               print("Growing up there was never enough of \(resources).")
-        childhoodMalaya()
-        //    tellJoke(setup: "What do you call a bear with no teeth?")
-    
-    
-        }
+    func childhoodMalaya() {
+        print(". My entire existence I have been running away from evil.  I ran the fastest from Brutus: evil personified, and somehow my father. Despite having 4 older siblings I was the only one he took his anger out on.  I was rebellious because I loved adventuring outdoors. There were just so many possibilities in the world. I could be so much more if I could just get out of these 4 walls my family calls home. n my many adventures I would often be up to mischievous things. I spent too much time upset and enraged when I had the chance to explore I just wanted to laugh.")
     }
     
     
-//    enum Choice {
-//        case nice
-//        case mean
-//    }
-//    func servant(currentChoice:Choice){
-//        switch currentChoice {
-//        case .nice:
-//            print("you live to see another day")
-//        case .mean:
-//            print("you get poisined")
-//        }
-//    }
-//    servant(currentChoice: .nice)
+    if let resources = resources.randomElement() {
+        print("This Zombie apocalypse is very triggering for me to say the least. The lack of resources makes me think about my childhood.Growing up there was never enough of \(resources).")
+    }
+            childhoodMalaya()
+        print("Let me tell you a joke")
+                
+                for _ in jokes {
+                    print ("\(jokes) \(Gummy)")
+                }
+    //write about special item and gender norms, and parents influence
+    
+    print()
+    enum Schedule {
+        case sevenAM,eightAM,eightthirtyAM,nineAM, tenAM, elevenAM, twelveAM, onePM, onethirtyPM, threethirtyPM, fivePM, sixPM, sevenPM, seventhrityPM, ninePM
+    }
+    func showSchedule(currentSchedule: Schedule) {
+        switch currentSchedule {
+        case .sevenAM:
+            print("Wake up")
+        case .eightAM:
+            print("Every morning I wake up and I have breakfast cooking group")
+        case .eightthirtyAM:
+            print("Breakfast")
+        case .nineAM:
+            print("Morning break")
+        case .tenAM:
+            print("Gardening")
+        case .elevenAM:
+            print("Individual Therapy")
+        case .twelveAM:
+            print("Lunch cooking group")
+        case .onePM :
+            print("Lunch")
+        case .onethirtyPM :
+            print("afternoon break")
+        case .threethirtyPM :
+            print("Arts & crafts")
+        case .fivePM :
+            print("Group therapy")
+        case .sixPM :
+            print("Dinner cooking group")
+        case .sevenPM :
+            print("Dinner")
+        case .seventhrityPM :
+            print("night time break")
+        case .ninePM :
+            print("Bedtime")
+        }
+    }
+//    showSchedule(currentSchedule: .eightAM)
+    
+    
+        
+    
+    
+     showSchedule(currentSchedule: .eightAM)
+    print("Before I leave my room I always recite a poem.")
+                
+                            enum Choice {
+                                case upbeat
+                                case sad
+                
+                            }
+                            func choose(currentChoice:Choice){
+                                switch currentChoice{
+                                case .upbeat:
+                                    print("Life is for the living. Death is for the dead. Let life be like music. And death a note unsaid.")
+                                case .sad:
+                                    print("What happens to a dream deferred? Does it dry up like a raisin in the sun? Or fester like a sore— And then run? Does it stink like rotten meat? Or crust and sugar over— like a syrupy sweet? Maybe it just sags like a heavy load. Or does it explode?")
+                                }
+                            }
+                choose(currentChoice:.sad)
+
+    
+        
+    
+    
+    
+    
+    
+       
 
 
+    
+    //
+    //    }
+    //    introduceMalaya()
+       
+    //
+    
+ 
+        
+        
+
+        //    var evenNumbers = [2, 4, 6, 8]
+        //    var specialItems = ["corset","belt","scrunchie"]
+        
+        
+        
+        
+        
+        //    enum Day { case sunday, monday, tuesday, wednesday, thursday, friday, saturday
+        //    }
+        //    func wknd(currentDay:Day){
+        //        switch currentDay {
+        //        case .sunday, .friday, .saturday:
+        //            print("true")
+        //        case .monday, .tuesday, .wednesday, .thursday:
+        //            print("false")
+        //        }
+        //    }
+        //    wknd(currentDay: .tuesday)
+    }
